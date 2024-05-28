9f6dce6bfe524d38371f4c5c96e2443a7c99778b
Samvardhan Singh <104402477+samvardhan03@users.noreply.github.com>
2024-05-28 13:58:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ import nltk
 nltk.download('all')
 import plotly.graph_objs as go
 from data_preprocessing import preprocess_text, extract_tickers
-from models import load_model, make_predictions
-from data_loader import load_stock_data_yfinance, engineer_features
+from data_loader import load_stock_data_yfinance
 
 def visualize_stock_data(stock_data, ticker):
     fig = go.Figure()
@@ -47,23 +46,6 @@ def main():
 
             for ticker in tickers:
                 visualize_stock_data(stock_data[ticker], ticker)
-
-            if len(tickers) > 1:
-                stock_data = engineer_features(stock_data)
-
-                # Load model
-                model = load_model()
-
-                # Make predictions
-                X_new = stock_data[['returns', 'SMA_20', 'SMA_50', 'RSI']].values
-                predictions = make_predictions(model, X_new)
-
-                # Display recommendations
-                st.write("Based on your query and the current market conditions, we recommend the following investments:")
-                for ticker, prediction in zip(tickers, predictions):
-                    st.write(f"{ticker}: {prediction}")
-            else:
-                st.write(f"You queried for the stock: {tickers[0]}. Please enter multiple tickers to get investment recommendations.")
 
 if __name__ == "__main__":
     main()
