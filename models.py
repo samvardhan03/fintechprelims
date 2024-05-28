cf51e475885ba3932a82575bbedb839a0d27d6d7
SirNiksALot <83578131+SirNiksALot@users.noreply.github.com>
2024-05-28 13:01:22 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,15 +1,18 @@
-
 import pickle
 import numpy as np
 from sklearn.ensemble import RandomForestRegressor
+import os
 
 def load_model():
-    with open('models/model.pkl', 'rb') as f:
-        model = pickle.load(f)
+    model_path = 'models/model.pkl'
+    if os.path.isfile(model_path):
+        with open(model_path, 'rb') as f:
+            model = pickle.load(f)
+    else:
+        # Create a new model instance if the file is not found
+        model = RandomForestRegressor()
     return model
 
 def make_predictions(model, X_new):
     predictions = model.predict(X_new)
     return predictions
-
-
