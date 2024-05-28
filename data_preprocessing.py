ac3f1b4347b187ad2f77b6349ce21c27d4757f2f
Samvardhan Singh <104402477+samvardhan03@users.noreply.github.com>
2024-05-28 13:50:44 +0530

    Update data_preprocessing.py

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,15 +1,14 @@
+
 import re
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
-from nltk.stem import PorterStemmer
 
 stop_words = set(stopwords.words('english'))
-stemmer = PorterStemmer()
 
 def preprocess_text(text):
     text = re.sub(r'[^a-zA-Z\s]', '', text)
     tokens = word_tokenize(text.lower())
-    filtered_tokens = [stemmer.stem(token) for token in tokens if token not in stop_words]
+    filtered_tokens = [token for token in tokens if token not in stop_words]
     return ' '.join(filtered_tokens)
 
 def extract_tickers_from_query(text):
@@ -21,3 +20,11 @@ def is_valid_ticker(ticker):
     # Add additional validation rules here
     # For now, we'll consider any uppercase string with optional spaces as a valid ticker
     return bool(re.match(r"^[A-Z]+(?:\s*[a-zA-Z]*)*$", ticker))
+
+def is_stock_query(text):
+    stock_keywords = ['stock', 'stocks', 'shares', 'investment', 'invest', 'buy', 'sell', 'trade']
+    tokens = text.split()
+    for token in tokens:
+        if token in stock_keywords:
+            return True
+    return False
